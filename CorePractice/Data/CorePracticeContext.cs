f0e565a7ed4d646a04dbba1db8a198a018a50e2e
Brad <bjkapteyn@gmail.com>
2019-07-01 22:43:12 -0400

    First commit

---
+++ CorePractice/Data/CorePracticeContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CorePractice.Models
+{
+    public class CorePracticeContext : DbContext
+    {
+        public CorePracticeContext (DbContextOptions<CorePracticeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CorePractice.Models.Movie> Movie { get; set; }
+    }
+}
